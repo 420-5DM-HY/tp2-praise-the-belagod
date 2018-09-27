d9a701cc7100c94557bb00565717bfe4ea3cb08f
SirPugington <GBourque2@outlook.com>
2018-09-26 20:23:21 -0400

    Début gestion audio
    J'ai trouver la façon de jouer de l'audio à partir d'un url qui est supposée marcher, mais l'app me sort une erreur vraiment vague et même après plusieurs essais je trouve vraiment pas pourquoi. À suivre...

--- app/src/main/java/com/example/gbour/tp2/MainActivity.java
+++ app/src/main/java/com/example/gbour/tp2/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.gbour.tp2;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "http://www.podtrac.com/pts/redirect.mp3/cdn.twit.tv/audio/sn/sn0679/sn0679.mp3";
+        MediaPlayer mp = new MediaPlayer();
+        try {
+            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mp.setDataSource(url);
+            mp.prepare();
+            mp.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
